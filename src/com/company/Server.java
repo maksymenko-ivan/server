1f4dded50eea6ac170c80336ad9486dc61545691
maksymenko-ivan <ivan.maksymenko.01@gmail.com>
2024-04-25 17:10:01 +0200

    Server created

---
+++ src/com/company/Server.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Server {
+    static final int PORT = 9290;
+    private ArrayList<ClientHandler>clients = new ArrayList<ClientHandler>();
+    public Server(){
+        Socket clientSocket = null;
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(PORT);
+            System.out.println("Server started");
+            while(true){
+                clientSocket= serverSocket.accept();
+                ClientHandler client = new ClientHandler(clientSocket,this);
+                clients.add(client);
+                new Thread(client).start();
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                clientSocket.close();
+                System.out.println("Server closed");
+                serverSocket.close();
+            }catch (IOException ex){
+                ex.printStackTrace();
+
+            }
+        }
+    }
+    public void sendMessageToAllClients(String msg){
+        for(ClientHandler o: clients){
+            o.sendMsg(msg);
+        }
+    }
+
+    public void removeClient(ClientHandler client) {
+        clients.remove(client);
+    }
+}
+
+
